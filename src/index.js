8949310cbc1ec09c355f551d002cf1538367152f
Gabriel Lemos <jmjgb@outlook.com>
2023-04-17 04:12:01 -0300

    Validação da rota /login criada

--- src/index.js
+++ src/index.js
@@ -17,26 +17,44 @@ app.get('/', (_request, response) => {
 const getFile = async () => {
   const file = await fs.readFile(path.resolve(__dirname, './talker.json'));
   return JSON.parse(file);
-}
+};
 
 app.get('/talker', async (req, res) => {
   const register = await getFile();
   return res.status(200).json(register);
-})
+});
 
 app.get('/talker/:id', async (req, res) => {
   const register = await getFile();
   const foundReg = register.find((obj) => obj.id === Number(req.params.id));
-  console.log(foundReg)
+  console.log(foundReg);
   if (!foundReg) {
-    return res.status(404).json({ message: "Pessoa palestrante não encontrada" });
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   return res.status(200).json(foundReg);
 });
 
-app.post('/login', (req, res) => {
+const validateTalker = (req, res, next) => {
+  const { email, password } = req.body;
+  console.log(email);
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length <= 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+app.post('/login', validateTalker, (req, res) => {
   const token = cryptoRS.randomBytes(8).toString('hex');
-  return res.status(200).json({ token: token });
+  return res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
