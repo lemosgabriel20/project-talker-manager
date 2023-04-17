05d52615a9c6b8e4a3452948b6f4283b59f70a99
Gabriel Lemos <jmjgb@outlook.com>
2023-04-17 01:53:45 -0300

    Rota para /talker criada

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs').promises;
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -10,6 +12,12 @@ const PORT = process.env.PORT || '3001';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', async (req, res) => {
+  const file = await fs.readFile(path.resolve(__dirname, './talker.json'));
+  const register = JSON.parse(file);
+  return res.status(200).json(register);
+})
 
 app.listen(PORT, () => {
   console.log('Online');
